683cc8c8e4b8d96038b3fd0778e856cbcb535604
m-fujishiro <fujishiro.business@gmail.com>
2020-01-17 10:59:09 +0900

    出力微調整

--- bittersweet.bat
+++ bittersweet.bat
@@ -12,7 +12,8 @@ echo …うん、大丈夫みたい。 & pause > nul
 echo そっかぁ…逃げられたんだ…。 & pause > nul
 echo …よかった。 & pause > nul
 echo …これでよかった、んだよね？ & pause > nul
-echo …。 & pause > nul
+echo …。 & pause > nul & echo.
+
 if %addict% gtr 0 (
   echo …やっぱり、耐えられない…。 & pause > nul
   echo 貴女の足が悪魔の元へ向かう。 & pause > nul & echo.
@@ -20,11 +21,12 @@ if %addict% gtr 0 (
   echo "どうしたの？大丈夫、ここまでおいで。" & pause > nul
   echo. & pause > nul
   echo "――おかえり。" & pause > nul & echo.
-  echo 嗚呼、何故私は離れようとしたのだろう。このまま溺れても、いいんじゃないかな…？ & pause > nul
+  echo ――嗚呼、何故私は離れようとしたのだろう。このまま溺れても、いいんじゃないかな…？ & pause > nul
+  echo. & pause > nul
   exit /b 0
 ) else (
   echo …うん。これで、よかったの。 & pause > nul
   echo これが、私の決めた道。私が私として歩む道。 &  pause > nul & echo.
 )
-echo Bitter End... & pause
+echo ～Bitter End～ & echo. & pause
 exit
